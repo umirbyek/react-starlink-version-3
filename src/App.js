71d4eaa7b80e66efb447a037f5090cd829d7b854
umirbyek <umirbyek0731@gmail.com>
2024-12-04 17:42:27 +0800

    fd

--- src/App.js
+++ src/App.js
@@ -60,9 +60,9 @@ function App() {
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
   // Төхөөрөмжийг шалгах код: iOS болон Android-ийг шалгах
   if (/android/i.test(userAgent)) {
-    window.parent.location.href = "emartmn://productdetail?skucd=bg?catid=782"; // Android-ийн апп линк, барааны SKU код бүхий
+    window.parent.location.href = "emartmn://productdetail?skucd=2140057847000"; // Android-ийн апп линк, барааны SKU код бүхий
   } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-    window.parent.location.href = "emartmn://productdetail?skucd=bg?catid=782"; // iOS-ийн апп линк, барааны SKU код бүхий
+    window.parent.location.href = "emartmn://productdetail?skucd=2140057847000"; // iOS-ийн апп линк, барааны SKU код бүхий
     console.log('User Agent:', userAgent);
 console.log('Attempting to redirect to:', window.parent.location.href);
   } else {
@@ -101,21 +101,7 @@ const specificationsClickMini=()=>{
   "https://emartmall.mn/productdetail/2140058771007"; 
 }
 const handleOrderClickStarlinkMain=()=>{
-
-  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-  // Төхөөрөмжийг шалгах код: iOS болон Android-ийг шалгах
-  if (/android/i.test(userAgent)) {
-    window.parent.location.href = "emartmn://productdetail?skucd=2140057847000"; // Android-ийн апп линк, барааны SKU код бүхий
-  } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-    window.parent.location.href = "emartmn://productdetail?skucd=2140057847000"; // iOS-ийн апп линк, барааны SKU код бүхий
-    console.log('User Agent:', userAgent);
-console.log('Attempting to redirect to:', window.parent.location.href);
-  } else {
-    // Вэб хуудасны линк ашиглах
-    window.parent.location.href = "https://emartmall.mn/productdetail/2140057847000";
-  }
-
-
+  window.parent.location.href="https://emartmall.mn/productdetail/2140057847000";
 }
   return (
     <div className="w-full h-screen">
